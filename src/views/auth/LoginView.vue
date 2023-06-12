022188dcc724e7c24868212635611e139121f1de
nestorsamson <nestorsamson99@gmail.com>
2023-06-12 08:01:09 +0000

    background color menu update

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -3,6 +3,9 @@
   <section class="bg-gray-50 dark:bg-gray-900">
     <div class="flex justify-center ">
       <div class="bg-white w-96 shadow dark:border bg-cover rounded px-8 pt-8 pb-8 ">
+        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white mb-">
+                  Login
+        </h1>
         <form @submit.prevent="authStore.handleLogin">
           <img/>
           <div>
@@ -21,7 +24,7 @@
               </span>
               <input type="password" name="password" v-model="authStore.form.password" class="w-full max-w-xs mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-full sm:text-sm focus:ring-1" placeholder="password" />
             </label>
-            <div class="mt-2 space-x-24 ">
+            <div class="mt-2 space-x-10">
               <button class="text-blue-500 btn btn-ghost text-sm font-medium text-slate-700 h-5">Forgot password?</button>
               <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full">Login</button>
             </div>
--- src/views/components/NavigationView.vue
+++ src/views/components/NavigationView.vue
@@ -4,16 +4,44 @@
   <input id="my-drawer" type="checkbox" class="drawer-toggle" />
   <div class="drawer-content flex">
     <!-- Page content here -->
-    <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
-  </div> 
+    <label for="my-drawer" class="w-9">
+      <img src="../../assets/menu.svg"/>
+    </label>
+  </div>
   <div class="drawer-side">
+    
     <label for="my-drawer" class="drawer-overlay"></label>
-    <ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
+    <ul class="menu p-4 w-80 h-full bg-base-200 text-base-content bg-green-600">
       <!-- Sidebar content here -->
-    <li><router-link to="/">Home</router-link> </li>
-    <li><router-link to="/about">About</router-link> </li>
-    <li><router-link to="/login">Login</router-link> </li>
-    <li><router-link to="/register">Register</router-link></li>
+      <h5 class="text-left mb-3 font-semibold text-white uppercase dark:text-gray-400">Menu</h5>
+
+      <button type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
+        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        <span class="sr-only">Close menu</span>
+        
+    </button>
+    <li>
+      <router-link to="/" class="">
+        <img src="../../assets/home.svg" class="w-6"/>
+        <label>Home</label>
+      </router-link>
+    </li>
+    <li>
+      <router-link to="/about">
+        <img src="../../assets/about.svg" class="w-6"/>
+        About
+      </router-link> </li>
+    <li>
+      <router-link to="/login">
+        <img src="../../assets/login.svg" class="w-6"/>
+        Login
+      </router-link> </li>
+    <li>
+      <router-link to="/register">
+        <img src="../../assets/register.svg" class="w-6"/>
+        Register
+      </router-link>
+    </li>
       
     </ul>
   </div>
