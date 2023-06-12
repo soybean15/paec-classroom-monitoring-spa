a1a4d8b11cae2609ed5f14ac5f53899642dcbbd1
soybean15 <marlonpadilla1593@gmail.com>
2023-06-12 13:57:55 +0800

    added daisy ui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="flex">
+<div class="">
   <NavigationView/>
   <router-view/>
 </div>
--- src/views/components/NavigationView.vue
+++ src/views/components/NavigationView.vue
@@ -1,10 +1,24 @@
 <template>
-    <nav class="flex flex-col">
-      <router-link to="/">Home</router-link> 
-    <router-link to="/about">About</router-link> 
-    <router-link to="/login">Login</router-link> 
-    <router-link to="/register">Register</router-link>
-</nav>
+
+<div class="drawer">
+  <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex">
+    <!-- Page content here -->
+    <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
+  </div> 
+  <div class="drawer-side">
+    <label for="my-drawer" class="drawer-overlay"></label>
+    <ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
+      <!-- Sidebar content here -->
+    <li><router-link to="/">Home</router-link> </li>
+    <li><router-link to="/about">About</router-link> </li>
+    <li><router-link to="/login">Login</router-link> </li>
+    <li><router-link to="/register">Register</router-link></li>
+      
+    </ul>
+  </div>
+</div>
+
 </template>
 
 <script>
