a49eb349b1ce7cf758f3c20fabb5ba97898251f7
Jean Esteban <jean@pop-os.localdomain>
2023-06-15 22:42:42 +0800

    first

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
-  This is Home
+   <h1>This is homepage</h1>
 </template>
 
 <script>
 import { onMounted } from 'vue';
+import UserProfileView from './user/UserProfileView.vue';
 import { useAuthStore } from "@/store/auth";
 // @ is an alias to /src
 
 import CreateProfileVIewVue from "./user/CreateProfileVIew.vue";
 export default {
   name: "HomeView",
-  components: {},
+  components: {
+    UserProfileView
+  },
   setup() {
   
     const authStore = useAuthStore()
--- src/views/user/UserProfileView.vue
+++ src/views/user/UserProfileView.vue
@@ -1,7 +1,12 @@
 <template>
-  <div >
+  <div>
+      
+    <div class="flex h-4/6 w-54 bg-white">
+      <div>
+       <h1 class="text-gray-900">Hi</h1>
+      </div>
+    </div>
 
-    <!-- UserProfile Design -->
 
   </div>
 </template>
