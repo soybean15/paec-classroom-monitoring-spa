3751241ebfffa71ee02a29733930b5dba4920588
soybean15 <marlonpadilla1593@gmail.com>
2023-06-26 23:08:19 +0800

    added add and get subjects

--- src/views/admin/components/modals/user/SubjectView.vue
+++ src/views/admin/components/modals/user/SubjectView.vue
@@ -12,7 +12,7 @@
         <div class="px-3 ">
           
             <ul >
-                <li class="hover:bg-transparent py-0.5">
+                <li class="hover:bg-transparent py-0.5" >
                     <div class="collapse bg-white ">
                         <input type="checkbox"  />
                         
@@ -62,12 +62,14 @@
 
         <div onclick="my_modal_5.showModal()" class="absolute bottom-5 right-4 bg-stone-100 rounded-full p-2 shadow-lg cursor-pointer"><svg fill="#000000" width="30px" height="30px" viewBox="0 0 24 24" id="add-file-6" data-name="Flat Line" xmlns="http://www.w3.org/2000/svg" class="icon flat-line"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="secondary" d="M11.5,19A6.5,6.5,0,0,1,18,12.5V7L14,3H5A1,1,0,0,0,4,4V20a1,1,0,0,0,1,1h6.82A6.3,6.3,0,0,1,11.5,19Z" style="fill: #2ca9bc; stroke-width: 2;"></path><path id="primary" d="M16,19h4m-2-2v4M8,13h2m4-4H8" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path><path id="primary-2" data-name="primary" d="M12,21H5a1,1,0,0,1-1-1V4A1,1,0,0,1,5,3h9l4,4v6" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path></g></svg></div>
     </div>
-    <Modal2View><AddSubjectModal/></Modal2View>
+    <Modal2View><AddSubjectModal :settings="store.settings[0]"/></Modal2View>
 </template>
 
 <script>
 import Modal2View from '@/views/components/ChildModalView.vue'
 import AddSubjectModal from './component/AddSubjectModal.vue'
+import { onMounted } from 'vue'
+import { useUserStore } from '@/store/users'
 
 export default {
     props:['store'],
@@ -75,6 +77,16 @@ export default {
     components:{
         AddSubjectModal,
         Modal2View
+    },
+    setup(props){
+        const userStore = useUserStore()
+
+        onMounted(()=>{
+            userStore.getTeacherSubjects(props.store.settings[0])
+        })
+
+        return {userStore}
+
     }
 
 }
