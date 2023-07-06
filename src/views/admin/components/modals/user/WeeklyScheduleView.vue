d445d71b5058db65f481079b2613455be86191d2
soybean15 <marlonpadilla1593@gmail.com>
2023-07-06 15:41:16 +0800

    extract schedule

--- src/views/admin/components/modals/user/WeeklyScheduleView.vue
+++ src/views/admin/components/modals/user/WeeklyScheduleView.vue
@@ -2,8 +2,8 @@
     <div class="w-full h-full overflow-auto p-3">
         <div class="w-full justify-between flex ">
             <div class="" v-for="day in weekdays" :key="day">
-                <DayComponent  :hasTime="day==='Time'">{{ day }}</DayComponent>
-                
+                <DayComponent :hasTime="day === 'Time'">{{ day }}</DayComponent>
+
             </div>
         </div>
     </div>
@@ -13,28 +13,40 @@
 import DayComponent from './component/DayComponent'
 import { useScheduleStore } from '@/store/schedule'
 
-function extractSchedules(subjects){
-    let schedules = []
+function extractSchedules(subjects) {
+    let schedules = {
+        Sunday: [],
+        Monday: [],
+        Tuesday: [],
+        Wednesday: [],
+        Thursday: [],
+        Friday: [],
+        Saturday: [],
+
+    }
+
+
 
     subjects.forEach(subject => {
-        console.log(subject.schedules)
-        let item = JSON.parse(JSON.stringify(subject.schedules))
-        schedules = schedules.concat(item)
-        console.log(item)
-    
-        // subject.schedules.forEach(schedule=>{
-        //     const array = Object.values(schedule);
-        //     schedules.push(array)
+     
+       let item = JSON.parse(JSON.stringify(subject.schedules))
+      
+      item.forEach(schedule => {
+            schedules[schedule.day].push(schedule)
+            console.log(schedule.day)
+        })
 
-        // })
+  
+
     });
+
 
     return schedules
 
 }
 
 export default {
-    components:{
+    components: {
         DayComponent
     },
 
@@ -58,10 +70,10 @@ export default {
 
 
 
-     
-        return { 
+
+        return {
             weekdays,
-             }
+        }
 
     }
 }
